12ce195029c0d5fa987499c7168903f5865207dc
Ben Sharafian <benms64@gmail.com>
2017-11-01 17:16:43 -0700

    feat: prototype version

---
+++ index.js
@@ -0,0 +1,30 @@
+const twit = require('twit')
+const Koa = require('koa')
+const router = require('router')()
+const parser = require('koa-bodyparser')()
+const Ilp = require('koa-ilp')
+const plugin = require('ilp-plugin')()
+const ilp = new Ilp({ plugin })
+const app = new Koa()
+
+const creds = require('./creds.json')
+const twitter = new twit(creds)
+
+router.post('/tweet', ilp.paid({ price: 10000 }), async ctx => {
+  const { status } = ctx.request.body
+
+  await new Promise((resolve, reject) => {
+    twitter.post('statuses', { status }, (err, data, response) => {
+      if (err) reject(err)
+      resolve(response)
+    })
+  })
+
+  ctx.body = { success: true }
+})
+
+app
+  .use(parser)
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(8080)
